d783d1afc0522be871e8beabdf879d180e0c01b8
Ho Wing Yip <sploooosh884@gmail.com>
2024-10-07 01:26:52 +0800

    Remove extra /help handler + pre-generate help text

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -19,12 +19,4 @@ application = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).build()
 for feature in features.__all__:
   feature.add_handlers(application)
 
-async def send_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text("\n".join(
-    feature.help_str() for feature in features
-    if getattr(feature, "help_str")
-  ))
-
-application.add_handler(CommandHandler("help", send_help))
-
 application.run_polling()
